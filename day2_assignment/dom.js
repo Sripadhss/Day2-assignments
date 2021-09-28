037cb007f7138ae3fb31bebc8e0a9ad239f2c85e
Sripadhss <sripadh103@gmail.com>
2021-09-28 17:12:01 +0530

    Initial commit

---
+++ day2_assignment/dom.js
@@ -0,0 +1,14 @@
+let number = [5,78,45,9,10]
+number.sort((a,b) => a-b);
+console.log(number)
+//to find the lengths of an array
+let names = ['sripadh', 'sree', 'pranay', 'rahul']
+names.map(names=> console.log(names.length))
+
+
+
+
+/*
+let a='sree';
+let b='pranay';
+document.getElementById("name").innerHTML="the name of a is "+a+ " the name of b is "+b;*/
